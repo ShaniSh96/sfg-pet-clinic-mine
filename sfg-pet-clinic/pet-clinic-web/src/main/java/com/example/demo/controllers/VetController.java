e014663aa6e1d3a0ccc682a0c3affd3e7e452fb2
Shivani Shanishchara <sshanishchara@gmail.com>
2020-07-20 14:42:01 +0530

    Vet Index and controller added. closes #5

---
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/com/example/demo/controllers/VetController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
+	public String listVets() {
+		
+		return "vets/Index";
+	}
+}
