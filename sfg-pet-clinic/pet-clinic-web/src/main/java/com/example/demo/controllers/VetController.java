a6019863259f6034181c8c29bf7716a97032a05b
Shivani Shanishchara <sshanishchara@gmail.com>
2020-07-21 22:23:15 +0530

    closes #8

--- sfg-pet-clinic/pet-clinic-web/src/main/java/com/example/demo/controllers/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/com/example/demo/controllers/VetController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.VetService;
 
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+	
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+
 	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
 		
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/Index";
 	}
 }
