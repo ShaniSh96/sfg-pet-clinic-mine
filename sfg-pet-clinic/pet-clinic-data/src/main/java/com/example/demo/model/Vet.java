0fab54f4533d3b770309207086c108aaae8ece80
Shivani Shanishchara <sshanishchara@gmail.com>
2020-07-27 12:32:08 +0530

    speciality added

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/demo/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/demo/model/Vet.java
@@ -1,5 +1,18 @@
 package com.example.demo.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+	private Set<Speciality>  specialties;
+
+	public Set<Speciality> getSpecialties() {
+		return specialties;
+	}
+
+	public void setSpecialties(Set<Speciality> specialties) {
+		this.specialties = specialties;
+	}
+	
 	
 }
