2c66d80222e98db4052c093cd6c290ba0deee928
Shivani Shanishchara <sshanishchara@gmail.com>
2020-07-27 12:35:41 +0530

    closes #22

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/demo/model/Owner.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/demo/model/Owner.java
@@ -4,15 +4,33 @@ import java.util.Set;
 
 public class Owner extends Person{
 	private Set<Pet> pets;
-
+	private String address;
+	private String telephone;
+	private String city;
 	public Set<Pet> getPets() {
 		return pets;
 	}
-
 	public void setPets(Set<Pet> pets) {
 		this.pets = pets;
 	}
-	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
 	
 	
 }
