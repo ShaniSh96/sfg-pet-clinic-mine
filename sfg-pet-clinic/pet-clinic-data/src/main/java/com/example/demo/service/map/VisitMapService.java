1012d36c38dbf4d7af378e3e3fefae5fd6530b67
Shivani Shanishchara <sshanishchara@gmail.com>
2020-07-29 18:03:41 +0530

    closes #40

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/demo/service/map/VisitMapService.java
@@ -0,0 +1,50 @@
+package com.example.demo.service.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Visit;
+import com.example.demo.service.VisitService;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService{
+
+private final VisitService visitService;
+	
+	public VisitMapService(VisitService visitService) {
+		this.visitService = visitService;
+	}
+	
+	@Override
+	public Visit save(Visit object) {
+		
+		if(object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null ||
+				object.getPet().getOwner().getId() == null) {
+			throw new RuntimeException("Invalid Visit");
+		}
+		return super.save(object);
+	}
+	
+	@Override
+	public Set<Visit> findAll() {
+		return super.findAll();
+	}
+	
+	@Override
+	public Visit findById(Long id) {
+		return super.findById(id);
+	}
+	
+	@Override
+	public void delete(Visit visit) {
+		super.delete(visit);
+	}
+	
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+	
+
+}
